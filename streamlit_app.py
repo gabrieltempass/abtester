0cf69257ef69c5b0e0e07090bf0f4afd379870b7
Gabriel Tem Pass <41839984+gabrieltempass@users.noreply.github.com>
2021-11-23 21:30:12 -0300

    feat: Make a permutation test for means

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,80 +135,88 @@ print(min_sample)
 if option == 'Evaluate the statistical significance':
 	st.header('Statistical significance')
 
-	control_users = st.number_input(
-		label='Users in the control',
-		min_value=0,
-		value=30000,
-		step=1)
-
-	treatment_users = st.number_input(
-		label='Users in the treatment',
-		min_value=0,
-		value=30000,
-		step=1)
-
-	control_conversions = st.number_input(
-		label='Conversions from the control',
-		min_value=0,
-		value=1219,
-		step=1)
-
-	treatment_conversions = st.number_input(
-		label='Conversions from the treatment',
-		min_value=0,
-		value=1247,
-		step=1)
-
-	confidence_level = st.slider(
-	    label='Confidence level',
-	    min_value=70,
-	    max_value=99,
-	    value=95,
-	    format='%d%%',
-	    key='post-test')
-
-	confidence_level = confidence_level/100
-	alpha = 1 - confidence_level
-
-	if not(st.button('Calculate')):
-		st.stop()
-
 	def perm_fun(x, nA, nB):
 	    n = nA + nB
 	    idx_A = set(random.sample(range(n), nB))
 	    idx_B = set(range(n)) - idx_A
 	    return x.loc[idx_B].mean() - x.loc[idx_A].mean()
 
-	control_effect = control_conversions/control_users
-	treatment_effect = treatment_conversions/treatment_users
-	observed_diff = treatment_effect - control_effect
+	test = st.radio(
+	    label='Test',
+	    options=('Proportions', 'Means'),
+	    index=0)
 
-	conversion = [0]*(control_users + treatment_users)
-	conversion.extend([1]*(control_conversions + treatment_conversions))
-	conversion = pd.Series(conversion)
+	if test == 'Proportions':
 
-	perm_diffs = []
-	i = 100
-	my_bar = st.progress(0)
-	for percent_complete in range(i):
-		perm_diffs.append(perm_fun(
-			conversion,
-			control_users + control_conversions,
-			treatment_users + treatment_conversions))
-		my_bar.progress((percent_complete + 1)/i)
+		control_users = st.number_input(
+			label='Users in the control',
+			min_value=0,
+			value=30000,
+			step=1)
 
-	p_value = np.mean([diff > observed_diff for diff in perm_diffs])
+		treatment_users = st.number_input(
+			label='Users in the treatment',
+			min_value=0,
+			value=30000,
+			step=1)
 
-	if p_value <= alpha:
-		st.subheader(f'The difference is statistically significant')
-	else:
-		st.subheader(f'The difference is not statistically significant')
-	st.write(f'Control conversion: {control_effect:.2%}')
-	st.write(f'Treatment conversion: {treatment_effect:.2%}')
-	st.write(f'Observed difference: {observed_diff*100:+.2f} p.p. ({observed_diff/control_effect:+.2%})')
-	st.write(f'p-value: {p_value:.2f}')
+		control_conversions = st.number_input(
+			label='Conversions from the control',
+			min_value=0,
+			value=1219,
+			step=1)
 
-	code = f'''
+		treatment_conversions = st.number_input(
+			label='Conversions from the treatment',
+			min_value=0,
+			value=1247,
+			step=1)
+
+		confidence_level = st.slider(
+		    label='Confidence level',
+		    min_value=70,
+		    max_value=99,
+		    value=95,
+		    format='%d%%',
+		    key='post-test')
+
+		confidence_level = confidence_level/100
+		alpha = 1 - confidence_level
+
+		if not(st.button('Calculate')):
+			st.stop()
+
+		control_effect = control_conversions/control_users
+		treatment_effect = treatment_conversions/treatment_users
+		observed_diff = treatment_effect - control_effect
+
+		conversion = [0]*(control_users + treatment_users)
+		conversion.extend([1]*(control_conversions + treatment_conversions))
+		conversion = pd.Series(conversion)
+
+		perm_diffs = []
+		i = 100
+		my_bar = st.progress(0)
+		for percent_complete in range(i):
+			perm_diffs.append(perm_fun(
+				conversion,
+				control_users + control_conversions,
+				treatment_users + treatment_conversions))
+			my_bar.progress((percent_complete + 1)/i)
+
+		p_value = np.mean([diff > observed_diff for diff in perm_diffs])
+
+		st.subheader('Result')
+		if p_value <= alpha:
+			st.success('The difference is statistically significant')
+		else:
+			st.error('The difference is not statistically significant')
+		st.write(f'Control conversion: {control_effect:.2%}')
+		st.write(f'Treatment conversion: {treatment_effect:.2%}')
+		st.write(f'Observed difference: {observed_diff*100:+.2f} p.p. ({observed_diff/control_effect:+.2%})')
+		st.write(f'p-value: {p_value:.2f}')
+
+		code = f'''
 # Import the libraries
 import random
 import numpy as np
@@ -241,7 +249,7 @@ conversion = pd.Series(conversion)
 
 # Execute the permutation test
 perm_diffs = []
-for percent_complete in range(1000):
+for _ in range(1000):
     perm_diffs.append(perm_fun(
         conversion,
         control_users + control_conversions,
@@ -259,7 +267,115 @@ print(f'Control conversion: {{control_effect:.2%}}')
 print(f'Treatment conversion: {{treatment_effect:.2%}}')
 print(f'Observed difference: {{observed_diff*100:+.2f}} p.p. ({{observed_diff/control_effect:+.2%}})')
 print(f'p-value: {{p_value:.2f}}')
-	'''
+		'''
 
-	with st.expander('Show the code'):
-		st.code(code, language='python')
+		with st.expander('Show the code'):
+			st.code(code, language='python')
+
+	else:
+
+		uploaded_file = st.file_uploader("Choose a file")
+		if uploaded_file is not None:
+		    dataframe = pd.read_csv(uploaded_file)
+		    st.write('File uploaded. Preview of the first 10 rows:')
+		    st.dataframe(dataframe.head(10), height=1000)
+		else:
+			st.write('The file must have each row with a unique user. One column named "group", with the user classification as "control" or "treatment". And one column named "measurement", with the value of the metric for the respective user. Below is an example of how the file should look like:')
+			sample = pd.read_csv('data_sample.csv')
+			st.dataframe(sample, height=1000)
+
+		confidence_level = st.slider(
+		    label='Confidence level',
+		    min_value=70,
+		    max_value=99,
+		    value=95,
+		    format='%d%%',
+		    key='post-test')
+
+		confidence_level = confidence_level/100
+		alpha = 1 - confidence_level
+
+		if not(st.button('Calculate')):
+			st.stop()
+
+		measurements = dataframe['measurement']
+		control_users = dataframe[dataframe['group'] == 'control'].shape[0]
+		treatment_users = dataframe[dataframe['group'] == 'treatment'].shape[0]
+
+		control_mean = dataframe[dataframe['group'] == 'control']['measurement'].mean()
+		treatment_mean = dataframe[dataframe['group'] == 'treatment']['measurement'].mean()
+		observed_diff = treatment_mean - control_mean
+
+		perm_diffs = []
+		i = 100
+		my_bar = st.progress(0)
+		for percent_complete in range(i):
+			perm_diffs.append(perm_fun(
+				measurements,
+				control_users,
+				treatment_users))
+			my_bar.progress((percent_complete + 1)/i)
+
+		p_value = np.mean(perm_diffs > observed_diff)
+
+		st.subheader('Result')
+		if p_value <= alpha:
+			st.success('The difference is statistically significant')
+		else:
+			st.error('The difference is not statistically significant')
+		st.write(f'Control mean: {control_mean:.2f}')
+		st.write(f'Treatment mean: {treatment_mean:.2f}')
+		st.write(f'Observed difference: {observed_diff/control_mean:+.2%}')
+		st.write(f'p-value: {p_value:.2f}')
+
+		code = f'''
+# Import the libraries
+import random
+import numpy as np
+import pandas as pd
+
+# Declare the permutation function
+def perm_fun(x, nA, nB):
+    n = nA + nB
+    idx_A = set(random.sample(range(n), nB))
+    idx_B = set(range(n)) - idx_A
+    return x.loc[idx_B].mean() - x.loc[idx_A].mean()
+
+# Define the parameters
+confidence_level = {confidence_level}
+alpha = 1 - confidence_level
+
+# Get the measurements and count the users
+measurements = dataframe['measurement']
+control_users = dataframe[dataframe['group'] == 'control'].shape[0]
+treatment_users = dataframe[dataframe['group'] == 'treatment'].shape[0]
+
+# Calculate the observed difference
+control_mean = dataframe[dataframe['group'] == 'control']['measurement'].mean()
+treatment_mean = dataframe[dataframe['group'] == 'treatment']['measurement'].mean()
+observed_diff = treatment_mean - control_mean
+
+# Execute the permutation test
+perm_diffs = []
+for _ in range(1000):
+    perm_diffs.append(perm_fun(
+        measurements,
+        control_users,
+        treatment_users))
+
+# Calculate the p-value
+p_value = np.mean(perm_diffs > observed_diff)
+
+# Show the result
+if p_value <= alpha:
+    print('The difference is statistically significant')
+else:
+    print('The difference is not statistically significant')
+print(f'Control mean: {{control_mean:.2f}}')
+print(f'Treatment mean: {{treatment_mean:.2f}}')
+print(f'Observed difference: {{observed_diff/control_mean:+.2%}}')
+print(f'p-value: {{p_value:.2f}}')
+		'''
+
+		with st.expander('Show the code'):
+			st.code(code, language='python')
