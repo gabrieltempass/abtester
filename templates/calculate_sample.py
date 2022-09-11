520a3237987beb53f7427f52ade09323c7c8d3d6
Gabriel Tem Pass <41839984+gabrieltempass@users.noreply.github.com>
2022-09-11 18:24:53 -0300

    feat: Update templates accordingly

--- templates/calculate_sample.py
+++ templates/calculate_sample.py
@@ -1,61 +1,58 @@
 # Import the libraries
 import math
-{% if test == 'Proportions' %}
+{% if test == "Proportions" %}
 import statsmodels.stats.api as sms
-{% elif test == 'Means' %}
+{% elif test == "Means" %}
 import pandas as pd
 from scipy.stats import norm
 {% endif %}
 
 # Define the parameters
-{% if test == 'Proportions' %}
+{% if test == "Proportions" %}
 control_conversion = {{ control_conversion }}
 {% endif %}
 sensitivity = {{ sensitivity }}
-{% if test == 'Proportions' %}
+{% if test == "Proportions" %}
 treatment_conversion = control_conversion * (1 + sensitivity)
 {% endif %}
-alternative = {% if test == 'Proportions' %}'{{ alternative }}'
-{% elif test == 'Means' %}'two-sided'
+{% if test == "Proportions" %}
+alternative = "{{ alternative }}"
 {% endif %}
 confidence_level = {{ confidence_level }}
 alpha = 1 - confidence_level
 power = {{ power }}
-{% if test == 'Means' %}
-beta = 1 - power
-
-# Count the users and get the standard deviation
-control_users = dataframe[dataframe['group'] == 'control'].shape[0]
-treatment_users = dataframe[dataframe['group'] == 'treatment'].shape[0]
-total_users = (control_users + treatment_users)
-std = dataframe[dataframe['group'] == 'control']['measurement'].std()
+{% if test == "Means" %}
+control_ratio = {{ control_ratio }}
+treatment_ratio = 1 - control_ratio
+std_dev = df["measurement"].std()
 {% endif %}
 
 # Calculate the minimum sample
-{% if test == 'Proportions' %}
+{% if test == "Proportions" %}
 effect_size = sms.proportion_effectsize(
     control_conversion,
     treatment_conversion
 )
 analysis = sms.TTestIndPower()
-sample = math.ceil(analysis.solve_power(
+treatment_sample = math.ceil(analysis.solve_power(
     effect_size,
     power=power,
     alpha=alpha,
     ratio=1,
     alternative=alternative
 ))
-{% elif test == 'Means' %}
-q0 = control_users/total_users
-q1 = treatment_users/total_users
-z_alpha = norm.ppf(1 - alpha/2)
+control_sample = treatment_sample
+{% elif test == "Means" %}
+z_alpha = norm.ppf(1 - alpha / 2)
 z_beta = norm.ppf(1 - beta)
-a = 1/q1 + 1/q0
+a = 1 / control_ratio + 1 / treatment_ratio
 b = pow(z_alpha + z_beta, 2)
-sample = math.ceil(a*b/pow(sensitivity/std, 2))
+total_sample = math.ceil(a * b / pow(sensitivity / std_dev, 2))
+control_sample = math.ceil(total_sample * control_ratio)
+treatment_sample = math.ceil(total_sample * treatment_ratio)
 {% endif %}
 
 # Show the result
-print(f'Minimum sample for the control group: {sample}')
-print(f'Minimum sample for the treatment group: {sample}')
-print(f'Total minimum sample for the experiment: {sample * 2}')
+print(f"Minimum sample for the control group: {control_sample}")
+print(f"Minimum sample for the treatment group: {treatment_sample}")
+print(f"Total minimum sample for the experiment: {control_sample + treatment_sample}")
--- templates/evaluate_significance.py
+++ templates/evaluate_significance.py
@@ -11,7 +11,7 @@ def permutation(x, nA, nB):
     return x.loc[idx_B].mean() - x.loc[idx_A].mean()
 
 # Define the parameters
-{% if test == 'Proportions' %}
+{% if test == "Proportions" %}
 control_users = {{ control_users }}
 treatment_users = {{ treatment_users }}
 control_conversions = {{ control_conversions }}
@@ -19,60 +19,62 @@ treatment_conversions = {{ treatment_conversions }}
 {% endif %}
 confidence_level = {{ confidence_level }}
 alpha = 1 - confidence_level
-{% if test == 'Means' %}
+{% if test == "Means" %}
 
 # Get the measurements and count the users
-measurements = dataframe['measurement']
-control_users = dataframe[dataframe['group'] == 'control'].shape[0]
-treatment_users = dataframe[dataframe['group'] == 'treatment'].shape[0]
+measurements = df["measurement"]
+control_users = df[df["group"] == "control"].shape[0]
+treatment_users = df[df["group"] == "treatment"].shape[0]
 {% endif %}
 
 # Calculate the observed difference
-{% if test == 'Proportions' %}
-control_effect = control_conversions/control_users
-treatment_effect = treatment_conversions/treatment_users
+{% if test == "Proportions" %}
+control_effect = control_conversions / control_users
+treatment_effect = treatment_conversions / treatment_users
 observed_diff = treatment_effect - control_effect
 
 # Create the pool to draw the samples
-conversion = [0]*(control_users + treatment_users)
-conversion.extend([1]*(control_conversions + treatment_conversions))
+conversion = [0] * (control_users + treatment_users)
+conversion.extend([1] * (control_conversions + treatment_conversions))
 conversion = pd.Series(conversion)
-{% elif test == 'Means' %}
-control_mean = dataframe[dataframe['group'] == 'control']['measurement'].mean()
-treatment_mean = dataframe[dataframe['group'] == 'treatment']['measurement'].mean()
+{% elif test == "Means" %}
+control_mean = df[df["group"] == "control"]["measurement"].mean()
+treatment_mean = df[df["group"] == "treatment"]["measurement"].mean()
 observed_diff = treatment_mean - control_mean
 {% endif %}
 
 # Execute the permutation test
 perm_diffs = []
 for _ in range(1000):
-    perm_diffs.append(permutation(
-        {% if test == 'Proportions' %}
-        conversion,
-        control_users + control_conversions,
-        treatment_users + treatment_conversions
-        {% elif test == 'Means' %}
-        measurements,
-        control_users,
-        treatment_users
-        {% endif %}
-    ))
+    perm_diffs.append(
+        permutation(
+            {% if test == "Proportions" %}
+            conversion,
+            control_users + control_conversions,
+            treatment_users + treatment_conversions
+            {% elif test == "Means" %}
+            measurements,
+            control_users,
+            treatment_users
+            {% endif %}
+        )
+    )
 
 # Calculate the p-value
-p_value = np.mean([diff > observed_diff for diff in perm_diffs])
+p_value = np.mean([diff > abs(observed_diff) for diff in perm_diffs])
 
 # Show the result
 if p_value <= alpha:
-    print('The difference is statistically significant')
+    print("The difference is statistically significant")
 else:
-    print('The difference is not statistically significant')
-{% if test == 'Proportions' %}
-print(f'Control conversion: {control_effect:.2%}')
-print(f'Treatment conversion: {treatment_effect:.2%}')
-print(f'Observed difference: {observed_diff*100:+.2f} p.p. ({observed_diff/control_effect:+.2%})')
-{% elif test == 'Means' %}
-print(f'Control mean: {control_mean:.2f}')
-print(f'Treatment mean: {treatment_mean:.2f}')
-print(f'Observed difference: {observed_diff/control_mean:+.2%}')
+    print("The difference is not statistically significant")
+{% if test == "Proportions" %}
+print(f"Control conversion: {control_effect:.2%}")
+print(f"Treatment conversion: {treatment_effect:.2%}")
+print(f"Observed difference: {observed_diff * 100:+.2f} p.p. ({observed_diff / control_effect:+.2%})")
+{% elif test == "Means" %}
+print(f"Control mean: {control_mean:.2f}")
+print(f"Treatment mean: {treatment_mean:.2f}")
+print(f"Observed difference: {observed_diff / control_mean:+.2%}")
 {% endif %}
-print(f'p-value: {p_value:.2f}')
+print(f"p-value: {p_value:.2f}")
