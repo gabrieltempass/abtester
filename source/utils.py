154c58bdc439adb5f7b92223c9af07a1284f57b5
Gabriel Tem Pass <41839984+gabrieltempass@users.noreply.github.com>
2024-05-02 13:47:43 -0300

    refactor: Remove unused function

--- source/utils.py
+++ source/utils.py
@@ -1,17 +1,7 @@
-import base64
 from typing import Union
 
-from jinja2 import FileSystemLoader, Environment
 import streamlit as st
-
-
-def render_svg(svg):
-    """Renders the given SVG string."""
-    b64 = base64.b64encode(svg.encode("utf-8")).decode("utf-8")
-    html = r'<img src="data:image/svg+xml;base64,%s" width="300" alt="abtester logo"/>' % b64
-    c = st.container()
-    c.write(html, unsafe_allow_html=True)
-    return
+from jinja2 import FileSystemLoader, Environment
 
 
 def add_spaces(n):
